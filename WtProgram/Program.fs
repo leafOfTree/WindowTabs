80f475e8d1cf8871373191d226a7ce6732cb73df
leafOfTree <node_github@163.com>
2025-02-17 09:14:02 +0800

    Bump version to 2025.02.17

--- WtProgram/Program.fs
+++ WtProgram/Program.fs
@@ -44,7 +44,7 @@ type ProgramVersion(parts:List2<int>)=
         this.compare(v2) > 0
 
 type Program() as this =
-    let version = "2024.11.13"
+    let version = "2025.02.17"
     let isStandAlone = System.Diagnostics.Debugger.IsAttached 
 
     let mutex = new Mutex(false, "BemoSoftware.WindowTabs")
